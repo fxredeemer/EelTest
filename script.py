61c440ccb334ef0f2a36741b5439dddec5f56d7d
Flurin Feuerstein <ffeuerstein@hamilton.ch>
2021-02-10 11:24:44 +0100

    basic working example with callback

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
 import eel
+import random
 
 eel.init('web')
+@eel.expose
+def send_command(x):
+    print('Hello from %s' % x)
+
+@eel.expose
+def py_random():
+    return random.random()
+
+
 eel.start('index.html')
