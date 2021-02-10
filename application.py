da1b4fff1709ae190f332bba1a88add32146e622
Flurin Feuerstein <ffeuerstein@hamilton.ch>
2021-02-10 17:38:58 +0100

    added state machine sample

---
+++ application.py
@@ -0,0 +1,22 @@
+from transitions import Machine
+from typing import List
+
+class Application(object):
+    states : List[str] = ["Starting", "Running", "Closing", "Aborted"]
+
+    state : str
+
+    def __init__(self) -> None:
+        self.machine = Machine(model=self, states=Application.states, initial="Starting")
+
+        self.machine.add_transition(trigger="go", source="Starting", dest="Running", before="do_run")
+
+        self.machine.add_transition(trigger="close", source="Running", dest="Closing")
+        
+        self.machine.add_transition(trigger="abort", source='*', dest='Aborted')
+
+    def do_run(self) -> None:
+        print("running")
+
+    def do_close(self) -> None:
+        print("closing")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from application import Application
 import eel
 import random
 
@@ -5,9 +6,19 @@ eel.init('web')
 @eel.expose
 def send_command(x):
     print('Hello from %s' % x)
+    try:
+        application.go()
+        eel.update_state(application.state) 
+    except:
+        eel.update_state("state machine error") 
 
 @eel.expose
 def py_random():
     return random.random()
+
+application = Application()
+
+print(application.state)
+eel.update_state(application.state) 
 
 eel.start('index.html')
